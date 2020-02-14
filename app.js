15b02a5bd58441895cbb3dab685aa65807bf28cd
Marudhu Pandiyan <marpandi@publicisgroupe.net>
2020-02-14 17:34:50 +0530

    test commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
@@ -47,4 +48,55 @@ app.get('/about', (req, res) => {
 
 app.listen(3000, ()=>{
     console.log('server is up on port 3000');
+=======
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const fetch = require('node-fetch');
+const app = express();
+
+
+console.log(__dirname)
+const directoryPath = path.join(__dirname, './public');
+const viewPath = path.join(__dirname, './templates/views');
+const partialPath = path.join(__dirname, './templates/partials');
+
+
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialPath);
+
+app.use(express.static(directoryPath));
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Marudhu'
+    })
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'help'
+    })
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('Error page')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'about'
+    })
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('server is up on port 3000');
+>>>>>>> Initial commit
 })
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 console.log('Client side server is up...');
 let fetchdata = 0;
 fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
@@ -5,4 +6,13 @@ fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
         fetchdata = data.userId;
         console.log('Fetch data:', data);
     })
+=======
+console.log('Client side server is up...');
+let fetchdata = 0;
+fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
+    response.json().then((data) => {
+        fetchdata = data.userId;
+        console.log('Fetch data:', data);
+    })
+>>>>>>> Initial commit
 })
